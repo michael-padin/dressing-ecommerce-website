4850a243bb89827a2a5d6ed439f5709884fa79e2
unknown <padinmichael201@gmail.com>
2022-07-17 19:34:41 +0800

    remove unecessary comment

--- client/src/api/shopApi.js
+++ client/src/api/shopApi.js
@@ -8,7 +8,3 @@ export const publicRequest = axios.create({
     baseURL: BASE_URL
 })
 
-// export const userRequest = axios.create({
-//     baseURL: BASE_URL,
-//     header: {token: `${TOKEN}`}
-// })
