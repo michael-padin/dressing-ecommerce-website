9c4f09ffefa427c05cdd03cd3af3f769839f4f4e
unknown <padinmichael201@gmail.com>
2022-08-02 19:06:57 +0800

    prep for deployment

--- client/src/api/shopApi.js
+++ client/src/api/shopApi.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-require('dotenv').config()  
 
-
-const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+const BASE_URL = "https://kaela-shop.herokuapp.com";
 // const TOKEN  = process.env.TOKEN;
 export const publicRequest = axios.create({
     baseURL: BASE_URL
