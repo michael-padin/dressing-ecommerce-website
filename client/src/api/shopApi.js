cec4aa84ef5e4012aa640621b5d30c395f06a19a
unknown <padinmichael201@gmail.com>
2022-08-02 18:28:53 +0800

    deployment

--- client/src/api/shopApi.js
+++ client/src/api/shopApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
+require('dotenv').config()  
 
 
-
-const BASE_URL = "http://localhost:3001"
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 // const TOKEN  = process.env.TOKEN;
 export const publicRequest = axios.create({
     baseURL: BASE_URL
